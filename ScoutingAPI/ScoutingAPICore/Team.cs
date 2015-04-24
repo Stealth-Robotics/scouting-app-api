a4a8bf6be710df3571de1499798435a6ab788acf
einsteinsci <einsteinsci@gmail.com>
2015-04-24 07:34:47 -0700

    Initial Commit

---
+++ ScoutingAPI/ScoutingAPICore/Team.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScoutingAPICore
+{
+    public class Team
+    {
+		public int ID
+		{ get; set; }
+
+		public string Name
+		{ get; set; }
+
+		public string Location
+		{ get; set; }
+
+		public string Description
+		{ get; set; }
+    }
+}
