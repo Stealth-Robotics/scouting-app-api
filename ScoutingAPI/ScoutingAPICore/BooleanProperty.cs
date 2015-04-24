fd737c0fceaf225a53cc9cfb1144391a94768c8a
einsteinsci <einsteinsci@gmail.com>
2015-04-24 08:03:22 -0700

    Quick Commit

---
+++ ScoutingAPI/ScoutingAPICore/BooleanProperty.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace ScoutingAPICore
+{
+	public class BooleanProperty : IScoutedProperty
+	{
+		public string Header
+		{ get; set; }
+
+		public UIElement UIForm()
+		{
+			return new CheckBox();
+		}
+	}
+}
